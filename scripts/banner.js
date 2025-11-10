242f3542f90825adb0f7529cf43e9617f6e0024c
Christoffer Lukkarinen <christofferlukkarinen@gmail.com>
2025-11-10 16:41:45 +0100

    banner working now

--- scripts/banner.js
+++ scripts/banner.js
@@ -1,7 +1,12 @@
 function bannerFunction() {
-  document.querySelector('#banner').innerHTML = `
-  <image src='../media/banner.gif'></image>
+  document.querySelector('#banner-div').innerHTML = `
+  <image class='banner-image' src='../media/banner.gif'>
+   <a class='home-image' href='../main.html'><image src='../media/home-button.png'></a>
+  </image>
+ 
   `;
 }
 
 bannerFunction();
+
+//  
